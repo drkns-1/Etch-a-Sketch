161d410f53d51732b6ae9a814219b193a4f7e54b
drkns_ <58770188+drkns-1@users.noreply.github.com>
2022-01-06 19:13:03 +0000

    Small Change
    Added slider to control the amount of columns in the etch-a-sketch.

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 //define grid size
 
-let userInput = prompt("how many columns would you like?");
-let gridSize = userInput*userInput;
 
-// Log outcome of gridSize
-console.log(gridSize);
+// Slider
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value;
 
-// creates the EaS pixel
-const containter = document.getElementById("container").style.gridTemplateColumns = "repeat("+userInput+", 1fr)"; 
-const pixelBlock = document.createElement('div');
-pixelBlock.classList.add("pixel");
-container.append(pixelBlock);
-
-// loop to relvant pixel amount
-for(i = 1; i < gridSize; i++){
-    container.append(pixelBlock.cloneNode(true));
-    
+slider.oninput = function() {
+    output.innerHTML = this.value;
 }
 
 
+// Removes all the squares when you change the grid size
+function reset(){
+    let container = document.getElementById("container");
+    container.innerHTML = "";
+    gridBuild();
+
+}
 
 
-// function colourChange () {
-//     pixelBlock.style.add("backgroundColor: black"); 
-// }
+function gridBuild(){
+    userInput = slider.value;
+    let gridSize = userInput*userInput;
+    // creates the EaS pixel
+    const containter = document.getElementById("container").style.gridTemplateColumns = "repeat("+userInput+", 1fr)"; 
+    const pixelBlock = document.createElement('div');
+    pixelBlock.classList.add("pixel");
 
-// let x = document.getElementsByClassName("pixel"); 
-// x.addEventListener("click", colourChange); 
+    container.append(pixelBlock);
+    // loop to relvant pixel amount
+    for(i = 1; i < gridSize; i++){
+        container.append(pixelBlock.cloneNode(true)); 
+    }
+ 
+}
+
